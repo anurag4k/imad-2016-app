699609dee9cb45d28f64a4a96941f0f7d68e6f8b
anurag4k <4438anurag@gmail.com>
2016-09-23 23:17:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/article-one.html',function(req,res){
-    res.sendFile(path.join(_dirname,'ui','article-one.html'));
+app.get('/a1.html',function(req,res){
+    res.sendFile(path.join(_dirname,'ui','a1.html'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
